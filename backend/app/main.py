6a1693cfcb99add4e93a8937e0c546d2297f5c98
Bianca Iusztin <bianca_iusztin91@yahoo.com>
2026-05-23 16:47:28 +0300

    Configure Alembic migrations and fix local email verification flow

--- backend/app/main.py
+++ backend/app/main.py
@@ -20,6 +20,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
+        "http://localhost:3001",
         "http://192.168.101.13:3000",
         "https://nutrition-application.vercel.app",
     ],
